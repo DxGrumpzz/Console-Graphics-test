b75af4a6039479d475cd4bae4729257ed9cbe0a4
Josef Lintz <yosi1.lints@gmail.com>
2020-06-30 13:37:52 +0300

    + Added '==' operator to Colour

--- ConsoleGraphicsTest/Colour.hpp
+++ ConsoleGraphicsTest/Colour.hpp
@@ -1,9 +1,23 @@
 #pragma once
 #include <stdint.h>
 
-struct Colour
+class Colour
 {
+public:
+
     uint8_t Red;
     uint8_t Green;
     uint8_t Blue;
+
+public:
+
+    bool operator == (const Colour& colour)
+    {
+        if ((Red == colour.Red) &&
+            (Green == colour.Green) &&
+            (Blue == colour.Blue))
+            return true;
+        else
+            return false;
+    };
 };
